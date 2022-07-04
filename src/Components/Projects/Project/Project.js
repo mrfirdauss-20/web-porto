b267e5c7cd5759a1612b7bac057f8d457969724b
Muhammad Risqi Firdaus <69805783+mrfirdauss-20@users.noreply.github.com>
2022-07-04 11:20:20 +0700

    fix buiild production

--- src/Components/Projects/Project/Project.js
+++ src/Components/Projects/Project/Project.js
@@ -3,7 +3,7 @@ import SecondaryButton from '../../secondaryButton';
 const Project = ({image, title, desc,url}) =>{
     return(
     <div className={style.card}>
-        <img className={style.img} src={image}/>
+        <img  alt="showcase" className={style.img} src={image}/>
         <h1 className={style.title}>{title}</h1>
         <p className={style.description}>{desc}</p> 
         {url ? <SecondaryButton text="Click here" link={url}/> : null}
