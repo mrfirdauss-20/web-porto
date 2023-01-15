0687cf795de4eedb58e8ae6483d0067489d68003
Muhammad Risqi Firdaus <risqi.firdaus20@gmail.com>
2023-01-15 18:29:37 +0700

    feat; update achivement

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -10,7 +10,7 @@ const Hero = () => {
                 <hr />
                 <div className={styles.contact}>
                     <p>
-                    <a href='https://github.com/mrfirdauss-20'>Github</a> | <a href='https://gitlab.com/mrfirdauss-20'>Gitlab</a> | <a href='https://www.kaggle.com/mrfirdauss20'>Kaggle</a> <br></br> <a href='https://www.linkedin.com/in/mrfirdauss/'>Linkedin</a> | <a href='https://coretgabut.blogspot.com/'>Blog</a>
+                    <a href='https://github.com/mrfirdauss-20'>Github</a> | <a href='https://gitlab.com/mrfirdauss-20'>Gitlab</a> | <a href='https://www.kaggle.com/mrfirdauss20'>Kaggle</a> <br></br> <a href='https://www.linkedin.com/in/mrfirdauss/'>Linkedin</a> | <a href='https://mrfirdauss.medium.com/'>Blog</a>
                     </p>
                 </div>
                 <div className={styles.btnCont}>
@@ -26,7 +26,7 @@ const Hero = () => {
                 <p>
                 An undergraduate informatic student who experienced in web and chatbot development. Eager to learn about developing technological things. Able to quickly learn and acclimate to new technologies. Besides that, he has a lot of experience being a leader in teams and national organizations.
                 </p>
-                <p><strong>Awards: </strong>Best Mentees (Top 1%) Telkom Digistar Class 2022, Top 6th Paragon Hackathon Competition 2022</p>
+                <p><strong>Awards: </strong>Gold Medalist Gemastik Data Mining Division 2022, Best Mentees (Top 1%) Telkom Digistar Class 2022, Top 10 Jabar Data Viz Fest 2022, Top 6 Paragon Hackathon Competition 2022</p>
             </div>
         </div>
     );
