dd2c816290094d89b0b06462a6c315933286c8e5
mrfirdauss <13520043@std.stei.itb.ac.id>
2023-11-30 05:11:07 +0700

    add analytics

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Components/NavBar';
 import Experiences from './Components/Experiences';
 import Projects from './Components/Projects';
 import Contact from './Components/Contact';
+import { Analytics } from '@vercel/analytics/react';
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
       <Experiences />
       <Projects />
       <Contact />
+      <Analytics />
     </div>
   );
 }
